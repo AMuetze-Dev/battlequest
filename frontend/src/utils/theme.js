82705fb24e4117b9d64d8bae4af569b2fdfb4f1d
Wayssm <aaron.muetze@outlook.com>
2023-09-08 12:57:22 +0200

    frontend theme update

--- frontend/src/utils/theme.js
+++ frontend/src/utils/theme.js
@@ -1,89 +1,26 @@
-const primaryDark = "17,17,17";
-const secondaryDark = "51,51,51";
-const contrast = "252, 163, 17";
-const secondaryLight = "229, 229, 229";
-const primaryLight = "255,255,255";
+const primaryLight = "240,240,240";
+const secondaryLight = "255,255,255";
+const tertiaryLight = "0,178,103";
+const textLight = "64,78,100";
+const subtextLight = "128,143,159";
 
-const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const primaryDark = "13,13,13";
+const secondaryDark = "26,26,26";
+const tertiaryDark = "221,82,1";
+const textDark = "255,255,255";
+const subtextDark = "87,87,87";
+
+const defaultDark = !window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 const root = document.documentElement;
-root.style.setProperty("--primary-dark", defaultDark ? primaryDark : primaryLight);
-root.style.setProperty("--secondary-dark", defaultDark ? secondaryDark : secondaryLight);
-root.style.setProperty("--contrast", defaultDark ? contrast : primaryDark);
-root.style.setProperty("--secondary-light", defaultDark ? secondaryLight : secondaryDark);
-root.style.setProperty("--primary-light", defaultDark ? primaryLight : primaryDark);
+root.style.setProperty("--primary", defaultDark ? primaryDark : primaryLight);
+root.style.setProperty("--secondary", defaultDark ? secondaryDark : secondaryLight);
+root.style.setProperty("--tertiary", defaultDark ? tertiaryDark : tertiaryLight);
+root.style.setProperty("--text", defaultDark ? textDark : textLight);
+root.style.setProperty("--subtext", defaultDark ? subtextDark : subtextLight);
 
-root.style.setProperty("--primary-dark-color", "rgb(var(--primary-dark))");
-root.style.setProperty("--secondary-dark-color", "rgb(var(--secondary-dark))");
-root.style.setProperty("--contrast-color", "rgb(var(--contrast))");
-root.style.setProperty("--secondary-light-color", "rgb(var(--secondary-light))");
-root.style.setProperty("--primary-light-color", "rgb(var(--primary-light))");
-
-const particlesOptions = {
-    "background": {
-        "color": `rgb(${defaultDark ? primaryDark : primaryLight})`
-    },
-    "interactivity": {
-        "events": {
-            "onClick": {
-                "enable": true,
-                "mode": "push"
-            },
-            "resize": true
-        },
-        "modes": {
-            "push": {
-                "quantity": 1,
-                "maxQuantity": 300
-            },
-            "repulse": {
-                "distance": 200,
-                "duration": 0.4
-            }
-        }
-    },
-    "particles": {
-        "color": {
-            "value": `rgb(${defaultDark ? secondaryDark : secondaryLight})`
-        },
-        "links": {
-            "color": `rgb(${defaultDark ? secondaryDark : secondaryLight})`,
-            "distance": 150,
-            "enable": true,
-            "opacity": 0.5,
-            "width": 1
-        },
-        "collisions": {
-            "enable": true
-        },
-        "move": {
-            "direction": "none",
-            "enable": true,
-            "outModes": {
-                "default": "bounce"
-            },
-            "random": true,
-            "speed": 0.5,
-            "straight": false
-        },
-        "number": {
-            "density": {
-                "enable": true
-            },
-            "value": 350,
-            "max": 400
-        },
-        "opacity": {
-            "value": 0.5
-        },
-        "shape": {
-            "type": "circle"
-        },
-        "size": {
-            "random": true,
-            "value": 5
-        }
-    }
-}
-
-export default particlesOptions;
+root.style.setProperty("--primary-color", "rgb(var(--primary))");
+root.style.setProperty("--secondary-color", "rgb(var(--secondary))");
+root.style.setProperty("--tertiary-color", "rgb(var(--tertiary))");
+root.style.setProperty("--text-color", "rgb(var(--text))");
+root.style.setProperty("--subtext-color", "rgb(var(--subtext))");
